9be408968b96cfc9bb830bfe3341087173319d85
David Fernández Chaves <davfercha@uma.es>
2021-10-05 13:06:26 +0200

    2.2.0

--- ROS.cs
+++ ROS.cs
@@ -137,7 +137,7 @@ namespace ROSUnityCore {
 
         private void Log(string _msg, LogLevel lvl, bool Warning = false)
         {
-            if (LogLevel <= lvl)
+            if (LogLevel <= lvl && LogLevel != LogLevel.Nothing)
             {
                 if (Warning)
                 {
